e9bf0e633fb7f793fc34f91da21ddf02c9afb21a
Allan Gadelha <allangadelha@dev.local>
2020-10-26 22:53:43 -0300

    sessão 10: componente nova tarefa 02

--- sessao-10-projeto-02-tarefas-todo/tarefas/src/components/NewTask.vue
+++ sessao-10-projeto-02-tarefas-todo/tarefas/src/components/NewTask.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="new-task">
-        <input v-model="name" @keydown.enter="add" type="text">
-        <button @click="add">+</button>
+        <input v-model="name" @keydown.enter="add" type="text" class="form-element" placeholder="Nova tarefa?">
+        <button class="form-element" @click="add">+</button>
     </div>    
 </template>
 
@@ -21,6 +21,30 @@ export default {
 }
 </script>
     
-<style >
-    
+<style>
+    .new-task {
+        margin: 35px;
+    }
+
+    .form-element {
+        border: 1px solid #FFF;
+        color: #FFF;
+        font-size: 2rem;
+        outline: none;
+        padding: 5px 10px 8px;
+    }
+
+    input.form-element {
+        background: #FFF2;
+        border-bottom-left-radius: 8px;
+        border-top-left-radius: 8px;
+        width: 500px; 
+    }
+
+    button.form-element {
+        background-color: #2196F3;
+        border-left: none;
+        border-bottom-right-radius: 8px;
+        border-top-right-radius: 8px;
+    }
 </style>
