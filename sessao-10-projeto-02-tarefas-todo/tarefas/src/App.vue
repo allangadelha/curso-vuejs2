0851c192f4d625dbcf0de2fbe4c4c896af058551
Allan Gadelha <allangadelha@dev.local>
2020-10-26 22:43:24 -0300

    sessão 10: componente nova tarefa 01

--- sessao-10-projeto-02-tarefas-todo/tarefas/src/App.vue
+++ sessao-10-projeto-02-tarefas-todo/tarefas/src/App.vue
@@ -1,21 +1,35 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<NewTask @taskAdded="addTask"/>
 		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
+import NewTask from './components/NewTask.vue'
 import TaskGrid from './components/TaskGrid.vue'
 
 export default {
-	components: { TaskGrid },
+	components: { NewTask, TaskGrid },
 	data() {
 		return {
 			tasks: [
 				{ name: 'Lavar louça', pending: false },
 				{ name: 'Comprar camisa', pending: true },
 			]
+		}
+	},
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if(reallyNew) {
+				this.tasks.push({
+					name: task.name,
+					pending:task.pending || true
+				})
+			}
 		}
 	}
 }
