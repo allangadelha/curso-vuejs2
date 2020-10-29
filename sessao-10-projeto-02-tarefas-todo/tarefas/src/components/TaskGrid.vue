1403aa3151ebae7b853dd083fb1cdb0de7824d02
Allan Gadelha <allangadelha@dev.local>
2020-10-28 21:58:30 -0300

    sessão 10: componente tarefa 03

--- sessao-10-projeto-02-tarefas-todo/tarefas/src/components/TaskGrid.vue
+++ sessao-10-projeto-02-tarefas-todo/tarefas/src/components/TaskGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="task-grid">
         <template v-if="tasks.length">
-            <Task v-for="(task, i) in tasks" :key="task.name" @taskDeleted="$emit('taskDeleted', i)" :task="task"></Task>
+            <Task v-for="(task, i) in tasks" :key="task.name" @taskDeleted="$emit('taskDeleted', i)" :task="task" @taskStateChanged="$emit('taskStateChanged', i)"></Task>
         </template>
         <p v-else>Sua vida está em dia :)</p>
     </div>
