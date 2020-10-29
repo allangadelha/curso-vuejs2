bf66c8f2e7c2a0b6cd3e40eb980f40b9ef32c9f5
Allan Gadelha <allangadelha@dev.local>
2020-10-28 22:30:36 -0300

    sessão 10: componente progresso

---
+++ sessao-10-projeto-02-tarefas-todo/tarefas/src/components/TasksProgress.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="tasks-progress">
+        <span class="progress-value">{{ progress }}%</span>
+        <div class="progress-bar" :style="{ width: progress + '%' }"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        progress: { type: Number, default: 0 }
+    }
+}
+</script>
+
+<style >
+    .tasks-progress {
+        align-items: center;
+        border: 1px solid #FFF;
+        border-radius: 8px;
+        color: #FFF;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        width: 80%;
+    }
+
+    .progress-bar {
+        align-self: flex-start;
+        background-color: #4CAF50;
+        border-radius: 8px;
+        height: 100%;
+        position: absolute;
+    }
+
+    .progress-value {
+        font-size: 1.5rem;
+        z-index: 1;
+    }
+</style>
