012558ccd595af58efa6fd5cb6de92b8de5b5cc1
Allan Gadelha <allangadelha@gmail.com>
2020-12-03 23:50:45 -0300

    sessão 14: criando transição Slide com propriedade CSS animation

--- sessao-14-adicionando-transicoes-animacoes/animacoes-exercicios/src/App.vue
+++ sessao-14-adicionando-transicoes-animacoes/animacoes-exercicios/src/App.vue
@@ -11,6 +11,9 @@
 		<transition name="fade">
 			<b-alert variante="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
+		<transition name="slide">
+			<b-alert variante="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
 	</div>
 </template>
 
@@ -43,5 +46,23 @@ export default {
 
 .fade-enter-active, .fade-leave-active {
 	transition: opacity 2s;
+}
+
+@keyframes slide-in {
+	from { transform: translateY(40px); }
+	to { transform: translateY(0); }
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0); }
+	to { transform: translateY(40px); }
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+}
+
+.slide-leave-active {
+	animation: slide-out 2s ease;
 }
 </style>
