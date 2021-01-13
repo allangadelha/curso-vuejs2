a29ad06c7c447b4be6ae9805c47287897f48b751
Allan Gadelha <allangadelha@gmail.com>
2021-01-12 22:39:32 -0300

    sessão 14: criando animação com Javascript

--- sessao-14-adicionando-transicoes-animacoes/animacoes-exercicios/src/App.vue
+++ sessao-14-adicionando-transicoes-animacoes/animacoes-exercicios/src/App.vue
@@ -35,7 +35,7 @@
 		</transition>
 
 		<hr>
-		<button @click="exibir2 = !exibir2">Mostrar</button>
+		<button @click="exibir2 = !exibir2">Alternar</button>
 		<transition
 			:css="false"
 			@before-enter="beforeEnter"
@@ -60,37 +60,52 @@ export default {
 			msg: 'Uma mensagem de informação para o usuário!',
 			exibir: false,
 			exibir2: true,
-			tipoAnimacao: 'fade'
+			tipoAnimacao: 'fade',
+			larguraBase: 0
 		}
 	},
 	methods: {
+		animar(el, done, negativo) {
+			let rodada = 1
+			const temporizador = setInterval(() => {
+				const novaLargura = this.larguraBase + 
+				(negativo ? -rodada * 10 : rodada * 10)
+				el.style.width = `${novaLargura}px`
+				rodada++
+				if(rodada > 30) {
+					clearInterval(temporizador)
+					done()
+				}
+			}, 20);
+
+		},
 		beforeEnter(el) {
-			console.log('beforeEnter')
+			this.larguraBase = 0
+			el.style.width = `${this.novaLargura}px`
 		},
 		enter(el, done) {
-			console.log('enter')
-			done()
+			this.animar(el, done, false)
 		},
-		afterEnter(el) {
-			console.log('afterEnter')
-		},
-		enterCancelled(el) {
-			console.log('enterCancelled')
-		},
+		// afterEnter(el) {
+		// 	console.log('afterEnter')
+		// },
+		// enterCancelled(el) {
+		// 	console.log('enterCancelled')
+		// },
 
 		beforeLeave(el) {
-			console.log('beforeLeave')
+			this.larguraBase = 300
+			el.style.width = `${this.novaLargura}px`
 		},
 		leave(el, done) {
-			console.log('leave')
-			done()
+			this.animar(el, done, true)
 		},
-		afterLeave(el) {
-			console.log('afterLeave')
-		},
-		leaveCancelled(el) {
-			console.log('leaveCancelled')
-		},
+		// afterLeave(el) {
+		// 	console.log('afterLeave')
+		// },
+		// leaveCancelled(el) {
+		// 	console.log('leaveCancelled') 
+		// },
 	}
 }
 </script>
